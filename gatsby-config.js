2f34220b580c63ddeb92dc0d2c2530f9c41bc43f
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-03 00:28:36 -0700

    init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const ROOT_PATH = path.resolve(__dirname, './src');
+
+module.exports = {
+  siteMetadata: {
+    title: 'Terry Chen | Software Engineer',
+    description: '',
+    author: 'Terry Chen',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        'common-ui': path.join(__dirname, 'src/components/commons/CommonUI.jsx'),
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${ROOT_PATH}/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${ROOT_PATH}/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+  ],
+};
