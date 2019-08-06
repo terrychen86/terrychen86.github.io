387bd674a3ccc9152fd9711c4f50cb5a467e5e6b
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-05 17:11:07 -0700

    refactr common-ui

---
+++ src/components/commons/CommonUI.jsx
@@ -0,0 +1,3 @@
+export { default as Container } from './Container/Container';
+export { default as NavItem } from './NavItem/NavItem';
+export { default as Module } from './Module/Module';
--- src/components/commons/Module/Module.jsx
+++ src/components/commons/Module/Module.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import './Section.scss';
+import './Module.scss';
 
 const Section = ({ className, ...props }) => {
   const { children } = props;
