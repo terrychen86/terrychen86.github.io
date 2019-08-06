03f8a8a7ef065fbf1abcfe8dfa8b8e0f711a94c4
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-05 17:10:29 -0700

    resolve alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,13 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '~components': path.resolve(__dirname, 'src/components'),
+        images: path.resolve(__dirname, 'src/images/'),
+        'common-ui': path.join(__dirname, 'src/components/commons/CommonUI.jsx'),
+      },
+    },
+  });
+};
