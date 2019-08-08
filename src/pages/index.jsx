51a1a5bd50e4cc4b5c10352ff6a8ecd7623822d7
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-08 02:17:00 -0700

    add about section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Layout from '../components/layout';
 
 import Hero from '../components/modules/index/Hero/Hero';
+import About from '../components/modules/index/About/About';
 
 const IndexPage = () => (
   <Layout>
     <Hero />
+    <About />
   </Layout>
 );
 
