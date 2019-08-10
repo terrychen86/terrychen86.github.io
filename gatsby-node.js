914370bb4e4b2cd5fb80dd224b280d9166cc314a
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-09 21:55:51 -0700

    fix path resolve

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,8 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        '~components': path.resolve(__dirname, 'src/components'),
-        images: path.resolve(__dirname, 'src/images/'),
+        components: path.resolve(__dirname, 'src/components'),
+        images: path.resolve(__dirname, 'src/images'),
         'common-ui': path.join(__dirname, 'src/components/commons/CommonUI.jsx'),
       },
     },
