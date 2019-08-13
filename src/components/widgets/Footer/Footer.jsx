0a0c92a710fafc109f50624cdf3590a429a9d973
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-12 22:30:47 -0700

    responsive UI

--- src/components/widgets/Footer/Footer.jsx
+++ src/components/widgets/Footer/Footer.jsx
@@ -5,9 +5,15 @@ import './Footer.scss';
 const Footer = () => (
   <section className="footer">
     <Container className="footer-container">
-      <p className="footer-text">
+      <p className="footer-text footer-text-desktop">
         Copyright (c) 2019 &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
         Website Designed and Built by&nbsp;
+        <strong>Terry Chen</strong>
+        &nbsp;&nbsp;|&nbsp;&nbsp; All right reserved.
+      </p>
+
+      <p className="footer-text footer-text-mobile">
+        Copyright (c) 2019 &nbsp;&nbsp;|&nbsp;&nbsp;by&nbsp;
         <strong>Terry Chen</strong>
         &nbsp;&nbsp;|&nbsp;&nbsp; All right reserved.
       </p>
