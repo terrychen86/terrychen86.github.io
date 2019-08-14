ce32512bab8bf773f56882c8a78f76fd8950ec72
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-13 23:51:04 -0700

    add fade in effect on About module

--- src/components/sections/index/About/About.jsx
+++ src/components/sections/index/About/About.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import classnames from 'classnames';
+import _ from 'lodash';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import { Container, Button } from 'common-ui';
 import profileImage from 'images/profile.png';
@@ -55,50 +56,74 @@ const renderIconCards = () => {
   ));
 };
 
+const isVisible = ({ ref, position }) => {
+  if (!ref || !ref.current) {
+    return false;
+  }
+  return ref.current.getBoundingClientRect().top < position;
+};
 
-const About = () => (
-  <main>
-    <section>
-      <Container className={styles.about}>
-        <div className={styles.aboutMe}>
-          <div className={styles.aboutMeImage}>
-            <img src={profileImage} alt="profile" />
+const About = () => {
+  const [windowTop, setWindowTop] = useState(window.innerHeight);
+  const contentRef = useRef(null);
+  const cardsRef = useRef(null);
+  useEffect(() => {
+    const scrollHandler = _.throttle(() => {
+      const viewport = +window.scrollY + window.innerHeight;
+      setWindowTop(viewport);
+    }, 10);
+
+    window.addEventListener('scroll', scrollHandler);
+    return () => { window.removeEventListener('scroll', scrollHandler); };
+  }, []);
+
+  const isContentVisible = isVisible({ ref: contentRef, position: windowTop });
+  const isCardsVisible = isVisible({ ref: cardsRef, position: windowTop });
+  console.log(isCardsVisible);
+  return (
+    <main>
+      <section>
+        <Container className={styles.about}>
+          <div className={classnames(styles.aboutMe, isContentVisible ? styles.fadeIn : '')} ref={contentRef}>
+            <div className={styles.aboutMeImage}>
+              <img src={profileImage} alt="profile" />
+            </div>
+
+            <div className={styles.aboutMeContent}>
+              <h3 className={styles.title}>
+                ABOUT ME
+              </h3>
+
+              <p className={styles.text}>
+                  Hi, my name is Terry (Yen-Hsuan).
+                  I write code and enjoy building beautiful and complicated things.
+                  I call many places home. I am from Taiwan,
+                  studied in LA and live in San Francisco now.
+                  I own both EE & CS master degrees, and I did scientific brain research too.
+                  I love travel, music, dogs, coffee, diet coke, and korean food!
+              </p>
+            </div>
           </div>
+        </Container>
+      </section>
 
-          <div className={styles.aboutMeContent}>
-            <h3 className={styles.title}>
-              ABOUT ME
-            </h3>
+      <section>
+        <Container ref={cardsRef} className={classnames(styles.cards, isCardsVisible ? styles.fadeIn : '')}>
+          {renderIconCards()}
+        </Container>
+      </section>
 
-            <p className={styles.text}>
-                Hi, my name is Terry (Yen-Hsuan).
-                I write code and enjoy building beautiful and complicated things.
-                I call many places home. I am from Taiwan,
-                studied in LA and live in San Francisco now.
-                I own both EE & CS master degrees, and I did scientific brain research too.
-                I love travel, music, dogs, coffee, diet coke, and korean food!
-            </p>
-          </div>
-        </div>
-      </Container>
-    </section>
-
-    <section>
-      <Container className={styles.cards}>
-        {renderIconCards()}
-      </Container>
-    </section>
-
-    <section>
-      <Container className={styles.btnContainer}>
-        <Link to="/projects">
-          <Button>
-            See my projects
-          </Button>
-        </Link>
-      </Container>
-    </section>
-  </main>
-);
+      <section>
+        <Container className={styles.btnContainer}>
+          <Link to="/projects">
+            <Button>
+              See my projects
+            </Button>
+          </Link>
+        </Container>
+      </section>
+    </main>
+  );
+};
 
 export default About;
