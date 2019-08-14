d044546cb677e60a915a5b96e2e4182000d213d5
Terry Chen <yenhsuan.terry@gmail.com>
2019-08-13 18:08:57 -0700

    header wrapper

--- src/components/sections/index/Hero/Hero.jsx
+++ src/components/sections/index/Hero/Hero.jsx
@@ -27,35 +27,37 @@ const Hero = () => {
   };
 
   return (
-    <Module className={styles.hero}>
-      <Container className={styles.container}>
-        <section className={styles.contentWrapper}>
-          <h1 className={styles.title}>Terry Chen</h1>
-          <h4 className={styles.subtitle}>Software Engineer @ ForUsAll</h4>
-          <p className={styles.text}>
-            Software Engineer who focuses on
-            building elegant and high performance
-            web application.
-          </p>
+    <header>
+      <Module className={styles.hero}>
+        <Container className={styles.container}>
+          <section className={styles.contentWrapper}>
+            <h1 className={styles.title}>Terry Chen</h1>
+            <h4 className={styles.subtitle}>Software Engineer @ ForUsAll</h4>
+            <p className={styles.text}>
+              Software Engineer who focuses on
+              building elegant and high performance
+              web application.
+            </p>
 
-          <div className={styles.links}>
-            <div className={styles.iconGroup}>
-              {renderIconLink()}
+            <div className={styles.links}>
+              <div className={styles.iconGroup}>
+                {renderIconLink()}
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        <section className={styles.imageWrapper}>
-          <img
-            src={heroImage}
-            alt="developer"
-            className={styles.image}
-          />
-        </section>
-      </Container>
+          <section className={styles.imageWrapper}>
+            <img
+              src={heroImage}
+              alt="developer"
+              className={styles.image}
+            />
+          </section>
+        </Container>
 
-      <Wave />
-    </Module>
+        <Wave />
+      </Module>
+    </header>
   );
 };
 
