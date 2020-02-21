027c591af80724960f7c0b61b91cd06a80465ed6
Terry Chen <yenhsuan.terry@gmail.com>
2020-02-20 22:24:33 -0800

    Refactor Index Hero

---
+++ src/components/styled-elements/fonts.jsx
@@ -0,0 +1,33 @@
+// @flow strict
+
+import styled from 'styled-components';
+import mediaQuery from 'utils/media-query';
+
+export const H1: React$ComponentType<{}> = styled.h1`
+  font-size: 54px;
+  font-family: 'Fredoka One', sans-serif;
+  font-weight: 500;
+
+  ${mediaQuery.below('sm')} {
+    font-size: 48px;
+  }
+`;
+
+export const H2: React$ComponentType<{}> = styled.h2`
+  font-size: 42px;
+  font-family: 'Fredoka One', sans-serif;
+  font-weight: 500;
+`;
+
+export const H4: React$ComponentType<{}> = styled.h4`
+  font-family: 'Avenir', 'Avenir Next', 'Lato', Helvetica, sans-serif;
+  font-size: 24px;
+
+  ${mediaQuery.below('sm')} {
+    font-size: 18px;
+  }
+`;
+
+export const P: React$ComponentType<{}> = styled.p`
+  font-family: 'Avenir', 'Avenir Next', 'Lato', Helvetica, sans-serif;
+`;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+// @flow strict
+
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { type IndexPageQuery } from 'types/graphql';
 import Layout from '../components/layout';
 
 import Hero from '../components/sections/index/Hero/Hero';
 import About from '../components/sections/index/About/About';
 
-const IndexPage = () => (
-  <Layout>
-    <Hero />
-    <About />
-  </Layout>
-);
+type Props = {|
+  data: IndexPageQuery,
+|};
 
-export default IndexPage;
+const Index = ({ data }: Props): React.Node => {
+  const iconLinks = data.allIconLinksJson.edges.map(e => e.node);
+
+  return (
+    <Layout>
+      <Hero iconLinks={iconLinks} />
+      <About />
+    </Layout>
+  );
+};
+
+export default Index;
+
+export const query = graphql`
+  query IndexPage {
+    allIconLinksJson {
+      edges {
+        node {
+          ...IconLink
+        }
+      }
+    }
+  }
+`;
