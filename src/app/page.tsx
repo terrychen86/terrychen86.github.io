feb94dd5d6837471d39e672e194ee3b1f9efeb27
Terry Chen <yenhsuan.terry@gmail.com>
2024-03-14 11:39:26 -0700

    Implement Hero & Intro sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import styles from "./page.module.scss";
 import Hero from "@/components/sections/hero/Hero";
+import Intro from "@/components/sections/intro/Intro";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main>
       <div className={styles.hero}>
         <Hero title="tbd" content="TBD" />
+      </div>
+
+      <div className={styles.intro}>
+        <Intro />
       </div>
     </main>
   );
