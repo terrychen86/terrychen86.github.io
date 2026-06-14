df94ee700217f8e47e31b8a4addad5176532ae71
Terry Chen <yenhsuan.terry@gmail.com>
2026-06-13 22:15:52 -0700

    Refactor portfolio foundation (#64)

---
+++ public/snake/js/game.js
@@ -0,0 +1,9 @@
+const snake = new Snake(125, 20, 20);
+
+document.getElementById("newGameBtn").addEventListener("click", () => {
+  snake.restartGame();
+});
+
+document.getElementById("newGameBtn").addEventListener("mousedown", (event) => {
+  event.preventDefault();
+});
